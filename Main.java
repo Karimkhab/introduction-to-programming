1aca0b7289baee716af010318507c1c3541f3c44
Karimkhab <164759389+Karimkhab@users.noreply.github.com>
2024-11-12 13:50:27 +0300

    Gomosek

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Pidorasina");
+        String in= "input.txt";
+        String out = "output.txt";
+        try {
+            FileReader fileReader = new FileReader(in);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            FileWriter fileWriter = new FileWriter(out);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+            bufferedReader.close();
+            bufferedWriter.close();
+        }catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }catch(IOException e){
+            System.out.println("I/O exception");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
